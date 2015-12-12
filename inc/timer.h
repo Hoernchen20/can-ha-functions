9f1f91cf7f962a37f05f4915ab4c670ca0170a84
Hoernchen20 <felix@horn-witten.de>
2015-12-12 18:16:54 +0100

    add blank files for weekly timer

---
+++ inc/timer.h
@@ -0,0 +1,42 @@
+﻿/**
+ * @file    timer.h
+ * @author  Felix Horn
+ * @version 0.1
+ * @date    12-Dec-2015
+ * @brief   This file contains defines and functions prototypes for
+ *          heating, ventilation and air condition.
+ *
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+/* Includes ----------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Exported constants ------------------------------------------------*/
+/* Exported variables ------------------------------------------------*/
+/* Exported types ----------------------------------------------------*/
+/* Exported macro ----------------------------------------------------*/
+/* Exported functions ------------------------------------------------*/
+
+#endif /* TIMER_H_ */
